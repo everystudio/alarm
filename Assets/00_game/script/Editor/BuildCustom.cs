018b32bc7a6b92b0a3d9e068c3f8caaaad8f0778
everystudio <watanabe.everystudio@gmail.com>
2016-06-29 02:03:03 +0900

    ビルド前後でファイル移動を指せる処理を追加

---
+++ Assets/00_game/script/Editor/BuildCustom.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using System.Collections;
+
+public class BuildCustom {
+
+	public static void PreBuild ()
+	{
+		#if UNITY_IOS
+		EditDirectory.AddCopy ("Assets/00_game/alarm.Resources/sound", "Assets/StreamingAssets");
+		#else
+		EditDirectory.AddCopy ("Assets/00_game/alarm.Resources/sound", "Assets/Plugins/Android/AN_Res/res/raw");
+		#endif
+		return;
+	}
+
+	public static void AfterBuild ()
+	{
+		#if UNITY_IOS
+		EditDirectory.Delete ("Assets/StreamingAssets");
+		#else
+		EditDirectory.Delete ("Assets/Plugins/Android/AN_Res/res/raw");
+		#endif
+		return;
+	}
+
+
+}
